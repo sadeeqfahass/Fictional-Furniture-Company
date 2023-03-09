cb60a4b880eccc3edd53bfcf2fb3e2633bb0af64
sadeeq fahass <sadeeqfhassan@gmail.com>
2023-03-09 13:01:44 +0100

    Add the side menu functionality and link bookmark

---
+++ app.js
@@ -0,0 +1,13 @@
+let menuIcon = document.getElementById("menu-icon")
+let menu = document.getElementById("menu")
+let checkBox = document.getElementById("check-box")
+
+
+menuIcon.addEventListener("click", () => {
+    menu.classList.toggle("nav-show")
+})
+
+document.addEventListener("scroll", () => {
+    menu.classList.remove("nav-show")
+    checkBox.checked = false
+})
