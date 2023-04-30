287a6d895256398da61162d330ff110a34464fc9
sadeeq fahass <sadeeqfhassan@gmail.com>
2023-04-30 12:04:12 +0100

    #

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
-let menuIcon = document.getElementById("menu-icon")
-let menu = document.getElementById("menu")
-let checkBox = document.getElementById("check-box")
-
+let menuIcon = document.getElementById("menu-icon");
+let menu = document.getElementById("menu");
+let checkBox = document.getElementById("check-box");
+let faqQuestions = document.querySelectorAll(".question");
+let faqIcons = document.querySelectorAll(".faq-icon");
 
 menuIcon.addEventListener("click", () => {
-    menu.classList.toggle("nav-show")
-})
+  menu.classList.toggle("nav-show");
+});
 
 document.addEventListener("scroll", () => {
-    menu.classList.remove("nav-show")
-    checkBox.checked = false
-})
+  menu.classList.remove("nav-show");
+  checkBox.checked = false;
+});
+
+faqQuestions.forEach((question) => {
+  question.addEventListener("click", () => {
+    question.nextElementSibling.classList.toggle("active");
+  });
+});
